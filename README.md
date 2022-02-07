7d21b0dc5ee84036926c140f3240606f0a7eec47
branherreria <branherreria@gmail.com>
2022-02-07 11:33:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-3nhngl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-3nhngl)
---
+++ src/data/MenuPaginas.js
@@ -0,0 +1,24 @@
+import Ejercicio1 from '../components/Ejercicio1';
+import Ejercicio2 from '../components/Ejercicio2';
+import Ejercicio3 from '../components/Ejercicio3';
+
+export const MenuPaginas = [
+  {
+    id: 1,
+    path: '/home',
+    title: 'HOME',
+    component: Home,
+  },
+  {
+    id: 2,
+    path: '/perfil',
+    title: 'PERFIL',
+    component: Perfil,
+  },
+  {
+    id: 3,
+    path: '/TablaApi',
+    title: 'TABLA',
+    component: TablaApi,
+  },
+];
